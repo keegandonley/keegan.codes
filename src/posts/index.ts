811284a2a4ab18a4180901e1bec433cec632a86f
Keegan Donley <kd@keegandonley.com>
2023-03-31 17:18:18 -0500

    fix some build

--- src/posts/index.ts
+++ src/posts/index.ts
@@ -2,8 +2,10 @@ import * as Welcome from "./welcome-to-my-new-blog.mdx";
 import * as Another from "./another-one.mdx";
 import * as OptionalChaining from "./optional-chaining.mdx";
 
-export default {
+const posts = {
   Welcome,
   Another,
   OptionalChaining,
 };
+
+export default posts;
