045c171efaa1ab59a538822a7b194fe0d2d65a2b
Keegan Donley <kd@keegandonley.com>
2023-04-01 15:17:24 -0500

    mobile view

--- src/components/MDXEntryRow/index.tsx
+++ src/components/MDXEntryRow/index.tsx
@@ -32,7 +32,7 @@ export const MDXEntryRow = ({
               src={`${BUCKET_URL}/${cover}`}
               alt="todo"
               fill
-              //   {...parseToProps(metadata)}
+              {...parseToProps(metadata)}
             />
           </div>
         ) : (
--- src/posts/index.ts
+++ src/posts/index.ts
@@ -1,11 +1,9 @@
 import * as Welcome from "./welcome-to-my-new-blog.mdx";
-import * as Another from "./another-one.mdx";
 import * as OptionalChaining from "./optional-chaining.mdx";
 import * as AppleShortcutsFitness from "./apple-shortcuts-fitness.mdx";
 
 const posts = {
   Welcome,
-  Another,
   OptionalChaining,
   AppleShortcutsFitness,
 };
