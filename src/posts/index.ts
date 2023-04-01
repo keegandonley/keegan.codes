475cea102ac331afef7389115e83eaa4e1551457
Keegan Donley <kd@keegandonley.com>
2023-04-01 16:43:05 -0500

    add holiday post

--- src/posts/index.ts
+++ src/posts/index.ts
@@ -2,12 +2,14 @@ import * as Welcome from "./welcome-to-my-new-blog.mdx";
 import * as OptionalChaining from "./optional-chaining.mdx";
 import * as AppleShortcutsFitness from "./apple-shortcuts-fitness.mdx";
 import * as ReviewYourPRs from "./review-your-own-pull-requests.mdx";
+import * as Holidays2022 from "./happy-holidays-22.mdx";
 
 const posts = {
   Welcome,
   OptionalChaining,
   AppleShortcutsFitness,
   ReviewYourPRs,
+  Holidays2022,
 };
 
 export default posts;
