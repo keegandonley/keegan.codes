8dc8b9d5d38c565af9069aeec28cf61a300efbc6
Keegan Donley <kd@keegandonley.com>
2023-04-01 16:23:07 -0500

    add PR review post

--- src/posts/index.ts
+++ src/posts/index.ts
@@ -1,11 +1,13 @@
 import * as Welcome from "./welcome-to-my-new-blog.mdx";
 import * as OptionalChaining from "./optional-chaining.mdx";
 import * as AppleShortcutsFitness from "./apple-shortcuts-fitness.mdx";
+import * as ReviewYourPRs from "./review-your-own-pull-requests.mdx";
 
 const posts = {
   Welcome,
   OptionalChaining,
   AppleShortcutsFitness,
+  ReviewYourPRs,
 };
 
 export default posts;
