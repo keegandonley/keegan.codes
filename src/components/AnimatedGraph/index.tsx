c7fbb82893da139bffeef237e906fe1859f4a501
Keegan Donley <kd@keegandonley.com>
2023-04-14 17:54:24 -0500

    reduce squares on mobile

--- src/components/AnimatedGraph/index.tsx
+++ src/components/AnimatedGraph/index.tsx
@@ -1,15 +1,37 @@
 import { merge } from "@/util/classNames";
 import styles from "./animatedGraph.module.css";
 import { BottomFade } from "../BottomFade";
+import { headers } from "next/headers";
 
-const values = Array(1500).fill(1);
+const toMatch = [
+  /Android/i,
+  /webOS/i,
+  /iPhone/i,
+  /iPad/i,
+  /iPod/i,
+  /BlackBerry/i,
+  /Windows Phone/i,
+];
 
 export const AnimatedGraph = () => {
+  const h = headers();
+  const userAgent = h.get("User-Agent");
+
+  // Normally detecting a user agent like this is not a good idea, but this is a
+  // simple performance optimization to avoid rendering a lot of squares.
+  // If we get it wrong, it's not a big deal.
+  const isLikelyMobile = toMatch.some((toMatchItem) => {
+    return userAgent ? userAgent.match(toMatchItem) : false;
+  });
+
+  const values = Array(isLikelyMobile ? 500 : 1500).fill(1);
+
   const coloredValues = values.map((_, i) => {
     const color = Math.floor(Math.random() * 5) + 1;
     const interval = Math.floor(Math.random() * 7) + 1;
     return { color, interval };
   });
+
   return (
     <div className={styles.container}>
       {coloredValues.map(({ color, interval }, i) => {
