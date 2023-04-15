60a9e15811fd625223488b0f8b7d0450059346b9
Keegan Donley <kd@keegandonley.com>
2023-04-15 00:51:28 -0500

    remove loader for now

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -5,11 +5,7 @@ export default function BlogLayout({ children, modal }: any) {
   return (
     <>
       {modal}
-      {/*
-        Manually doing suspense here instead of loading.tsx because it seems
-        to have some issues with parallel routes, and route interception.
-      */}
-      <Suspense fallback={<Loading />}>{children}</Suspense>
+      {children}
     </>
   );
 }
