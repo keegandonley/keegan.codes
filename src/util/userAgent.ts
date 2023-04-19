d219e0ad34f64f10e27b76367a364594bdb6330e
Keegan Donley <kd@keegandonley.com>
2023-04-19 18:18:49 -0500

    update comment

--- src/util/userAgent.ts
+++ src/util/userAgent.ts
@@ -11,8 +11,9 @@ const toMatch = [
 ];
 
 // Normally detecting a user agent like this is not a good idea, but this is a
-// simple performance optimization to avoid rendering a lot of squares.
-// If we get it wrong, it's not a big deal.
+// simple performance optimization used to render squares in the header,
+// or determine images above the fold. It should NOT be used for conditional, important UI,
+// such as a mobile menu. That way, if we get it wrong, it's not a big deal.
 export const getIsLikelyMobile = () => {
   const h = headers();
   const userAgent = h.get("User-Agent");
