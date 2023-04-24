cd75127260c1243dd8a2e8a6aae93af54dc2f09d
Keegan Donley <kd@keegandonley.com>
2023-04-24 17:31:43 -0500

    add og type

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -43,6 +43,7 @@ export async function generateMetadata({
             url: `/api/og/post?slug=${params.slug}`,
             width: 1200,
             height: 600,
+            type: "image/png",
           },
         ],
       },
