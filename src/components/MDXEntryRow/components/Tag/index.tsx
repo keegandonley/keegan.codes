2738bf638a762d6c0b1088b898bf3a7b7b78f9a7
Keegan Donley <kd@keegandonley.com>
2023-04-23 21:24:51 -0500

    add tags

--- src/components/MDXEntryRow/components/Tag/index.tsx
+++ src/components/MDXEntryRow/components/Tag/index.tsx
@@ -1,9 +1,28 @@
+"use client";
+
+import { MouseEvent, useCallback } from "react";
 import styles from "./tag.module.css";
+import { useRouter } from "next/navigation";
 
 interface TagProps {
   tag: string;
 }
 
 export const Tag = ({ tag }: TagProps) => {
-  return <span className={styles.tag}>{tag}</span>;
+  const router = useRouter();
+
+  const handleClick = useCallback(
+    (e: MouseEvent) => {
+      e.preventDefault();
+
+      router.push(`/blog/tag/${encodeURIComponent(tag)}`);
+    },
+    [router, tag]
+  );
+
+  return (
+    <button onClick={handleClick} className={styles.tag}>
+      {tag}
+    </button>
+  );
 };
