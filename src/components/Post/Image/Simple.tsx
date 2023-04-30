10a0ec733888c3424f6e3335b9b98bb5cc5a285e
Keegan Donley <kd@keegandonley.com>
2023-04-30 10:05:29 -0500

    fix image rendering

--- src/components/Post/Image/Simple.tsx
+++ src/components/Post/Image/Simple.tsx
@@ -1,25 +1,12 @@
-import Image from "next/image";
 import simpleStyles from "./simple.module.css";
-import { injectVariables, merge } from "@/util/classNames";
+import { merge } from "@/util/classNames";
 
-export const SimpleImg = ({ src, width, height, alt }: any) => {
+export const SimpleImg = ({ src, alt }: any) => {
   return (
-    <div
-      className={simpleStyles.parent}
-      style={injectVariables([
-        ["width", `${width}px`],
-        ["height", `${height}px`],
-      ])}
-    >
-      <Image
-        src={src}
-        alt={alt}
-        fill
-        className={merge(simpleStyles.img)}
-        style={{
-          filter: "drop-shadow(0 0 0.5rem var(--shadow-color))",
-        }}
-      />
+    <div className={simpleStyles.parent}>
+      {/* We don't want to try to optimize the og image, so just use a normal img tag */}
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img src={src} alt={alt} className={merge(simpleStyles.img)} />
     </div>
   );
 };
