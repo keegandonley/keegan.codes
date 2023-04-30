2250352e63e388b188888b64bdaa8851b8f01853
Keegan Donley <kd@keegandonley.com>
2023-04-30 00:08:28 -0500

    try to fade loader in

--- src/app/blog/@modal/(...)blog/[slug]/loading.tsx
+++ src/app/blog/@modal/(...)blog/[slug]/loading.tsx
@@ -1,30 +1,5 @@
-import { faSpinnerThird } from "@fortawesome/pro-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import styles from "./loading.module.css";
+import { Loader } from "@/components/Loader";
 
 export default function BlogModalLoading() {
-  return (
-    <div
-      style={{
-        position: "fixed",
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
-        zIndex: 999999,
-        display: "flex",
-        alignItems: "center",
-        fontSize: "12rem",
-      }}
-    >
-      <FontAwesomeIcon
-        icon={faSpinnerThird}
-        spin
-        style={{
-          margin: "0 auto",
-        }}
-        className={styles.icon}
-      />
-    </div>
-  );
+  return <Loader />;
 }
