da6be621af6663c9f92cf7e601aba22b397cdafa
Keegan Donley <kd@keegandonley.com>
2023-04-30 10:14:14 -0500

    improve scores

--- src/components/Post/Image/Simple.tsx
+++ src/components/Post/Image/Simple.tsx
@@ -1,12 +1,18 @@
 import simpleStyles from "./simple.module.css";
 import { merge } from "@/util/classNames";
 
-export const SimpleImg = ({ src, alt }: any) => {
+export const SimpleImg = ({ src, alt, height, width }: any) => {
   return (
     <div className={simpleStyles.parent}>
       {/* We don't want to try to optimize the og image, so just use a normal img tag */}
       {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img src={src} alt={alt} className={merge(simpleStyles.img)} />
+      <img
+        src={src}
+        alt={alt}
+        className={merge(simpleStyles.img)}
+        height={height}
+        width={width}
+      />
     </div>
   );
 };
