1eee29358267061c69bd5feea28166b51327750d
Keegan Donley <kd@keegandonley.com>
2023-04-29 23:19:23 -0500

    fix anchor tags

--- src/components/Post/Heading/H1/index.tsx
+++ src/components/Post/Heading/H1/index.tsx
@@ -2,11 +2,17 @@ import { merge } from "@/util/classNames";
 import { MDXComponentBaseProps } from "../../types";
 import h1Styles from "./h1.module.css";
 import hStyles from "../h.module.css";
+import { getId } from "../util";
 
 interface H1Props extends MDXComponentBaseProps {}
 
 export const H1 = ({ children, className }: H1Props) => {
   return (
-    <h1 className={merge(hStyles.h, h1Styles.h1, className)}>{children}</h1>
+    <h1
+      className={merge(hStyles.h, h1Styles.h1, className)}
+      id={getId(children)}
+    >
+      {children}
+    </h1>
   );
 };
--- src/components/Post/Heading/H3/index.tsx
+++ src/components/Post/Heading/H3/index.tsx
@@ -2,11 +2,17 @@ import { merge } from "@/util/classNames";
 import { MDXComponentBaseProps } from "../../types";
 import h3Styles from "./h3.module.css";
 import hStyles from "../h.module.css";
+import { getId } from "../util";
 
 interface H3Props extends MDXComponentBaseProps {}
 
 export const H3 = ({ children, className }: H3Props) => {
   return (
-    <h3 className={merge(hStyles.h, h3Styles.h3, className)}>{children}</h3>
+    <h3
+      className={merge(hStyles.h, h3Styles.h3, className)}
+      id={getId(children)}
+    >
+      {children}
+    </h3>
   );
 };
