4624e703a57d672dfe149c02b851d663b0d0a21f
Keegan Donley <kd@keegandonley.com>
2024-02-29 13:16:34 -0600

    make sure errors are consistent, so add a catchall route

---
+++ src/app/[shortCode]/[...rest]/page.tsx
@@ -0,0 +1,17 @@
+import { redirect } from "next/navigation";
+
+export const runtime = "edge";
+
+interface RestProps {
+  params: {
+    shortCode: string;
+    rest: string[];
+  };
+}
+
+export default async function ShortCodeRest(props: RestProps) {
+  const fullUrl = props.params.rest?.length
+    ? `${props.params.shortCode}/${props.params.rest.join("/")}`
+    : props.params.shortCode;
+  return redirect(`/routing-error?slug=${fullUrl}&type=page`);
+}
--- src/middleware/notFoundMiddleware.ts
+++ src/middleware/notFoundMiddleware.ts
@@ -12,15 +12,14 @@ export class NotFoundMiddleware {
   done() {
     const url = new URL(this.request.url);
     const slug = url.searchParams.get("slug");
-    const type = url.searchParams.get("type");
+    const type = url.searchParams.get("type") || "generic";
 
     const headers = new Headers(this.request.headers);
     if (slug) {
       headers.set("x-error-slug", slug);
     }
-    if (type) {
-      headers.set("x-error-type", type);
-    }
+
+    headers.set("x-error-type", type);
 
     return NextResponse.next({
       request: {
