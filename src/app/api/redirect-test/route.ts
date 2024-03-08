09fb653d39d548c0832fd91117d9302f7be1753d
Keegan Donley <kd@keegandonley.com>
2024-03-08 13:48:10 -0600

    update endpoints

--- src/app/api/redirect-test/route.ts
+++ src/app/api/redirect-test/route.ts
@@ -13,8 +13,15 @@ export async function GET() {
 }
 
 export async function POST() {
-  return Response.json({
-    message: "Hello, World!",
-    redirect_url: "https://keegan.codes/test-success",
-  });
+  return Response.json(
+    {
+      message: "Hello, World!",
+      redirect_url: "https://keegan.codes/test-success",
+    },
+    {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    }
+  );
 }
