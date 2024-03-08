400421f65a42393be6837356b57a23c337767a89
Keegan Donley <kd@keegandonley.com>
2024-03-08 13:54:18 -0600

    skip events

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,30 @@ import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
+const ignorePaths = ["/test-success"];
+
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
-      <Analytics />
-      <SpeedInsights />
+      <Analytics
+        beforeSend={(event) => {
+          if (ignorePaths.some((path) => event.url.endsWith(path))) {
+            return null;
+          }
+
+          return event;
+        }}
+      />
+      <SpeedInsights
+        beforeSend={(event) => {
+          if (ignorePaths.some((path) => event.url.endsWith(path))) {
+            return null;
+          }
+
+          return event;
+        }}
+      />
     </>
   );
 }
