d9500c73b019eecdc779d1fd2080df936c7c0bfc
Keegan Donley <kd@keegandonley.com>
2024-03-14 11:40:33 -0500

    add logging

--- src/app/api/test/parrot/route.ts
+++ src/app/api/test/parrot/route.ts
@@ -1,5 +1,12 @@
 import { cookies } from "next/headers";
 import { NextRequest } from "next/server";
+import { connect } from "@planetscale/database";
+
+const config = {
+  host: process.env.host,
+  username: process.env.username,
+  password: process.env.password,
+};
 
 export async function POST(request: NextRequest) {
   let result;
@@ -37,15 +44,34 @@ export async function POST(request: NextRequest) {
 
   const cookiesList = cookies();
 
+  const conn = connect(config);
+
+  const cookiesResult = cookiesList.getAll().reduce((acc, curr) => {
+    return {
+      ...acc,
+      [curr.name]: curr.value,
+    };
+  }, {});
+
+  try {
+    await conn.execute(
+      "INSERT INTO parrot_requests (payload, cookies, mode, origin, created_at) VALUES (?, ?, ?, ?, ?)",
+      [
+        JSON.stringify(result),
+        JSON.stringify(cookiesResult),
+        mode,
+        origin,
+        new Date().toISOString().slice(0, 19).replace("T", " "),
+      ]
+    );
+  } catch (ex) {
+    console.error("Failed to insert request", ex);
+  }
+
   return Response.json(
     {
       payload: result,
-      cookies: cookiesList.getAll().reduce((acc, curr) => {
-        return {
-          ...acc,
-          [curr.name]: curr.value,
-        };
-      }, {}),
+      cookies: cookiesResult,
       mode,
     },
     {
