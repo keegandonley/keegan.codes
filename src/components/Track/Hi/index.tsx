a515df7b578b3d35f392325840851b567a5b87b0
Keegan Donley <kd@keegandonley.com>
2024-03-28 16:11:15 -0500

    remove unneeded extra items

--- src/components/Track/Hi/index.tsx
+++ src/components/Track/Hi/index.tsx
@@ -5,13 +5,15 @@ import { useEffect } from "react";
 export const HiTrack = ({ qrScanned, slug }: HiTrackBody) => {
   useEffect(() => {
     if (process.env.NODE_ENV !== "development") {
-      fetch("/api/view/hi", {
-        method: "POST",
-        body: JSON.stringify({
-          slug: slug,
-          qrScanned,
-        } as HiTrackBody),
-      });
+      setTimeout(() =>
+        fetch("/api/view/hi", {
+          method: "POST",
+          body: JSON.stringify({
+            slug: slug,
+            qrScanned,
+          } as HiTrackBody),
+        })
+      );
     }
   }, [qrScanned, slug]);
 
--- src/components/Track/index.tsx
+++ src/components/Track/index.tsx
@@ -5,13 +5,15 @@ import { useEffect } from "react";
 export const Track = ({ inModal, slug }: TrackBody) => {
   useEffect(() => {
     if (process.env.NODE_ENV !== "development") {
-      fetch("/api/view", {
-        method: "POST",
-        body: JSON.stringify({
-          slug: slug,
-          inModal: inModal,
-        } as TrackBody),
-      });
+      setTimeout(() =>
+        fetch("/api/view", {
+          method: "POST",
+          body: JSON.stringify({
+            slug: slug,
+            inModal: inModal,
+          } as TrackBody),
+        })
+      );
     }
   }, [inModal, slug]);
 
