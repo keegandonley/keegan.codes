8b425503a7caa70772976d2863f73d7d9fd61592
Keegan Donley <kd@keegandonley.com>
2024-04-06 11:33:19 -0500

    use new utils from foundations for UA detection

--- src/util/userAgent.ts
+++ src/util/userAgent.ts
@@ -1,14 +1,5 @@
+import { isUserAgentLikelyMobile } from '@keegancodes/foundations';
 import { headers } from 'next/headers';
-
-const toMatch = [
-  /Android/i,
-  /webOS/i,
-  /iPhone/i,
-  /iPad/i,
-  /iPod/i,
-  /BlackBerry/i,
-  /Windows Phone/i,
-];
 
 // Normally detecting a user agent like this is not a good idea, but this is a
 // simple performance optimization used to render squares in the header,
@@ -18,7 +9,5 @@ export const getIsLikelyMobile = () => {
   const h = headers();
   const userAgent = h.get('User-Agent');
 
-  return toMatch.some((toMatchItem) => {
-    return userAgent ? userAgent.match(toMatchItem) : false;
-  });
+  return isUserAgentLikelyMobile(userAgent);
 };
