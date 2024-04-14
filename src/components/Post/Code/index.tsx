12b3531dd401d93fb4a917e777d7466ceb56402b
Keegan Donley <kd@keegandonley.com>
2024-04-14 11:24:52 -0500

    copy feature for code block

--- src/components/Post/Code/index.tsx
+++ src/components/Post/Code/index.tsx
@@ -1,6 +1,11 @@
+'use client';
+
 import { injectVariables, merge } from '@/util/classNames';
 import { GeistMono } from 'geist/font/mono';
 import styles from './code.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck, faCopy } from '@keegandonley/pro-solid-svg-icons';
+import { useCopyElementText } from '@keegancodes/foundations-react';
 
 interface CodeProps {
   children?: any;
@@ -8,6 +13,10 @@ interface CodeProps {
 }
 
 export const Code = ({ children, className }: CodeProps) => {
+  const language = className?.replace('language-', '');
+
+  const { ref, onClick, pending } = useCopyElementText();
+
   return (
     <code
       className={merge(className, styles.parent)}
@@ -16,7 +25,27 @@ export const Code = ({ children, className }: CodeProps) => {
         ['fstyle', GeistMono.style.fontStyle],
       ])}
     >
-      {children}
+      {language ? (
+        <span className={merge(styles.header)}>
+          <span className={styles.langText}>{language}</span>
+          <button
+            className={styles.button}
+            onClick={onClick}
+            disabled={pending}
+          >
+            <div
+              className={merge(
+                styles.clicker,
+                pending ? styles.clicked : styles.copy,
+              )}
+            >
+              <FontAwesomeIcon icon={faCheck} />
+              <FontAwesomeIcon icon={faCopy} />
+            </div>
+          </button>
+        </span>
+      ) : null}
+      <span ref={ref}>{children}</span>
     </code>
   );
 };
