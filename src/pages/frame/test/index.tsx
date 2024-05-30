32e352d2031eacb46fa23e931996632dde26c95b
Keegan Donley <kd@keegandonley.com>
2024-05-30 14:38:21 -0500

    add frame messaging test page

---
+++ src/pages/frame/test/index.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image';
+import Bender from '../../../images/bender.jpg';
+import '@/tw.css';
+import { useEffect, useRef } from 'react';
+
+export default function FrameTestPage() {
+  useEffect(() => {
+    console.log('sending init');
+    window.parent.postMessage('keegan_frame_test_init', '*');
+  }, []);
+
+  const interval = useRef<any>(null);
+
+  useEffect(() => {
+    interval.current = setInterval(() => {
+      console.log('sending tick');
+      window.parent.postMessage('keegan_frame_test_tick', '*');
+    }, 2000);
+
+    return () => clearInterval(interval.current);
+  });
+
+  return (
+    <div className="flex w-full flex-col items-center pt-12">
+      <h1 className="text-xl">Doing frame stuff</h1>
+      <Image width={300} src={Bender} alt="Bender" />
+    </div>
+  );
+}
