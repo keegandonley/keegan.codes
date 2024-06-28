12071a27ddc9e89dfbaa0789431050eb4733d1e0
Keegan Donley <kd@keegandonley.com>
2024-06-28 10:58:19 -0500

    change sentry sampling

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -4,8 +4,8 @@ Sentry.init({
   dsn: 'https://9e697d5ed9dc747d6a7fb2047836ce8b@o4507381392408576.ingest.us.sentry.io/4507381395423232',
   tracesSampleRate: 1,
   debug: false,
-  replaysOnErrorSampleRate: 1.0,
-  replaysSessionSampleRate: 0.5,
+  replaysOnErrorSampleRate: 1,
+  replaysSessionSampleRate: 0,
   integrations: [],
 });
 
