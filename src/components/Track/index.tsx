3e84f5f1e55467bdae943d574ceed1d0db04e4e6
Keegan Donley <kd@keegandonley.com>
2024-07-09 16:54:52 -0500

    Update index.tsx

--- src/components/Track/index.tsx
+++ src/components/Track/index.tsx
@@ -1,40 +1,21 @@
-import { getFullyQualifiedDeploymentUrl } from '@keegancodes/foundations-next';
-import { waitUntil } from '@vercel/functions';
-import { captureException } from '@sentry/nextjs';
-
-const trackView = async (slug: string, inModal: boolean) => {
-  if (process.env.NODE_ENV !== 'development') {
-    console.warn('trackView called for', slug, inModal);
-    try {
-      const { url, headers } =
-        await getFullyQualifiedDeploymentUrl(`/api/view`);
-      await fetch(url, {
-        method: 'POST',
-        body: JSON.stringify({
-          slug: slug,
-          inModal: inModal,
-        } as TrackBody),
-        priority: 'low',
-        headers,
-      });
-    } catch (ex) {
-      captureException(ex);
-    }
-  } else {
-    console.log('Skipping tracking in local dev, event would be', {
-      slug,
-      inModal,
-    });
-  }
-};
+"use client";
+import { useEffect } from 'react';
 
 const Track = ({ inModal, slug }: TrackBody) => {
-  if (typeof window === "undefined") {
-    console.warn('Calling track', Date.now());
-    waitUntil(trackView(slug, inModal));
-  }
-
-  return null;
+  useEffect(() => {
+    if (process.env.NODE_ENV !== "development") {
+      setTimeout(() =>
+        fetch("/api/view", {
+          method: "POST",
+          body: JSON.stringify({
+            slug: slug,
+            inModal: inModal,
+          } as TrackBody),
+          priority: "low",
+        })
+      );
+    }
+  }, [inModal, slug]);
 };
 
 export default Track;
