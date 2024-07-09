b628a7678d48e091cfd378c4adceac2927ca35f7
Keegan Donley <kd@keegandonley.com>
2024-07-09 09:11:48 -0500

    add some sitemap stuff that Bing apparently uses

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -10,35 +10,44 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     {
       url: 'https://keegan.codes',
       lastModified: published,
+      priority: 1,
     },
     {
       url: 'https://keegan.codes/resume',
       lastModified: published,
+      priority: 0.3,
     },
     {
       url: 'https://keegan.codes/blog',
       lastModified: published,
+      priority: 0.8,
+      changeFrequency: 'weekly',
     },
     {
       url: 'https://keegan.codes/pi',
       lastModified: published,
+      priority: 0.3,
     },
     ...Object.keys(Posts).map((key) => {
       return {
         url: `https://keegan.codes/blog/${(Posts as any)[key].slug}`,
         lastModified:
           (Posts as any)[key].updated || (Posts as any)[key].published,
+        priority: 0.7,
       };
     }),
     {
       url: 'https://keegan.codes/library',
       lastModified: published,
+      priority: 0.7,
+      changeFrequency: 'weekly',
     },
     ...Object.keys(Books).map((key) => {
       return {
         url: `https://keegan.codes/library/${(Books as any)[key].slug}`,
         lastModified:
           (Books as any)[key].updated || (Books as any)[key].published,
+        priority: 0.5,
       };
     }),
   ];
