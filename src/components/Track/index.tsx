4f1c014b478586376a2e6397a4648c5ec5f9da9b
Keegan Donley <kd@keegandonley.com>
2024-07-10 08:59:02 -0500

    switch to dynamic

--- src/components/Track/index.tsx
+++ src/components/Track/index.tsx
@@ -1,19 +1,24 @@
-"use client";
+'use client';
 import { useEffect } from 'react';
 
 const Track = ({ inModal, slug }: TrackBody) => {
   useEffect(() => {
-    if (process.env.NODE_ENV !== "development") {
+    if (process.env.NODE_ENV !== 'development') {
       setTimeout(() =>
-        fetch("/api/view", {
-          method: "POST",
+        fetch('/api/view', {
+          method: 'POST',
           body: JSON.stringify({
             slug: slug,
             inModal: inModal,
           } as TrackBody),
-          priority: "low",
-        })
+          priority: 'low',
+        }),
       );
+    } else {
+      console.log('Skipping tracking in local dev, event would be', {
+        slug,
+        inModal,
+      });
     }
   }, [inModal, slug]);
 
