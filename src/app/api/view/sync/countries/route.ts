0370490317b7623a5437a701aa0cebd505ca1e46
Keegan Donley <kd@keegandonley.com>
2024-07-16 09:55:08 -0500

    track the top countries by slug

---
+++ src/app/api/view/sync/countries/route.ts
@@ -0,0 +1,71 @@
+import { ExecutedQuery, connect } from '@planetscale/database';
+import { NextResponse } from 'next/server';
+
+const config = {
+  host: process.env.host,
+  username: process.env.username,
+  password: process.env.password,
+};
+
+const BATCH_SIZE = 20;
+
+export async function GET() {
+  const startTime = Date.now();
+  const conn = connect(config);
+
+  console.log('Processing top countries for slugs');
+
+  try {
+    const results: ExecutedQuery<{
+      slug: string;
+      views: string;
+      country_code: string;
+    }> = await conn.execute(
+      'select count(*) as views, slug, country_code from post_page_views WHERE country_code IS NOT NULL GROUP BY country_code, slug;',
+      [],
+    );
+
+    const groups: Record<
+      string,
+      Array<{ views: number; country_code: string }>
+    > = results.rows.reduce((acc, curr) => {
+      const res: Record<
+        string,
+        Array<{ views: number; country_code: string }>
+      > = { ...acc };
+
+      if (!res[curr.slug]) {
+        res[curr.slug] = [];
+      }
+
+      res[curr.slug].push({
+        views: parseInt(curr.views, 10),
+        country_code: curr.country_code,
+      });
+
+      return res;
+    }, {});
+
+    for (const slug in groups) {
+      const rows = groups[slug];
+      const sortedRows = rows.sort((a, b) => b.views - a.views);
+      const max = sortedRows[0];
+      await conn.execute(
+        'INSERT INTO slug_top_countries (slug, country_code) VALUES (?, ?) on duplicate key update country_code = ?',
+        [slug, max.country_code, max.country_code],
+      );
+    }
+
+    const endTime = Date.now();
+
+    const duration = endTime - startTime;
+
+    return NextResponse.json({
+      ok: true,
+      duration,
+    });
+  } catch (ex) {
+    console.error(ex);
+    return new Response(JSON.stringify(ex), { status: 500 });
+  }
+}
