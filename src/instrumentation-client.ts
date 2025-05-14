d2ac2a0d743616e11e77b635792fbb3eb694f07e
Keegan Donley <kd@keegandonley.com>
2025-05-14 09:26:33 -0500

    update sentry

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -4,7 +4,7 @@ Sentry.init({
   dsn: 'https://9e697d5ed9dc747d6a7fb2047836ce8b@o4507381392408576.ingest.us.sentry.io/4507381395423232',
   integrations: [],
   tracesSampleRate: 1.0,
-  replaysSessionSampleRate: 0.1,
+  replaysSessionSampleRate: 0,
   replaysOnErrorSampleRate: 1.0,
   debug: false,
   enabled: process.env.NODE_ENV === 'production',
