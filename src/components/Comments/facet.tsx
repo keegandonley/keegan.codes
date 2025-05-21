d1b61d1f9c93b1ad1509b08b112b2ef80c9a3b19
Keegan Donley <kd@keegandonley.com>
2025-05-21 09:49:41 -0500

    better keys

--- src/components/Comments/facet.tsx
+++ src/components/Comments/facet.tsx
@@ -8,17 +8,26 @@ export const replaceFacets = (content: string, facets: any) => {
   });
 
   const elements = [];
+  let linkCount = 0;
+  let mentionCount = 0;
+  let textCount = 0;
   for (const segment of Array.from(rt.segments())) {
     if (segment.isLink() && segment.link) {
+      linkCount += 1;
       elements.push(
-        <Link key={segment.link.uri} href={segment.link.uri} target="_blank">
+        <Link
+          key={`link-${segment.link.uri}-${linkCount}`}
+          href={segment.link.uri}
+          target="_blank"
+        >
           {segment.text}
         </Link>,
       );
     } else if (segment.isMention()) {
+      mentionCount += 1;
       elements.push(
         <Link
-          key={segment.mention?.did}
+          key={`mention-${segment.mention?.did}-${mentionCount}`}
           href={`https://bsky.app/profile/${segment.mention?.did}`}
           target="_blank"
         >
@@ -26,7 +35,10 @@ export const replaceFacets = (content: string, facets: any) => {
         </Link>,
       );
     } else {
-      elements.push(<span key={segment.text}>{segment.text}</span>);
+      textCount += 1;
+      elements.push(
+        <span key={`text-${segment.text}-${textCount}`}>{segment.text}</span>,
+      );
     }
   }
 
