b7b0ca075c87dd030730ba65f99a7e96fa888eae
Keegan Donley <kd@keegandonley.com>
2026-02-03 21:48:40 -0600

    proofread pass

--- src/components/Definition/index.tsx
+++ src/components/Definition/index.tsx
@@ -1,20 +1,50 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import defs from './definitions.json';
-import { faMagnifyingGlass } from '@keegandonley/pro-solid-svg-icons';
 import styles from './definition.module.css';
-import { faInfoCircle } from '@keegandonley/pro-regular-svg-icons';
+import { faQuestionCircle } from '@keegandonley/pro-regular-svg-icons';
+import { useId } from 'react';
+import localFont from 'next/font/local';
+
+const accentFont = localFont({
+  // src: './fonts/Domine.ttf',
+  src: '../../app/fonts/InstrumentSerif.ttf',
+});
 
 export const Definition = (props: { children: keyof typeof defs }) => {
-  const def = defs[props.children];
+  let def = defs[props.children];
+  let key = props.children;
+  const popoverId = useId();
 
-  if (!def) {
+  if ('see' in def && def.see) {
+    key = def.see as keyof typeof defs;
+    def = defs[def.see as keyof typeof defs];
+  }
+
+  if (
+    !('partOfSpeech' in def) ||
+    !('definition' in def) ||
+    !def.partOfSpeech ||
+    !def.definition
+  ) {
     return <>{props.children}</>;
   }
 
   return (
-    <abbr title={def} className={styles.wrapper}>
+    <span className={styles.wrapper}>
       {props.children}
-      <FontAwesomeIcon icon={faInfoCircle} className={styles.icon} />
-    </abbr>
+      <button
+        popoverTarget={popoverId}
+        className={styles.button}
+        aria-label={`Show definition for ${props.children}`}
+      >
+        <FontAwesomeIcon icon={faQuestionCircle} className={styles.icon} />
+      </button>
+      <span popover="auto" id={popoverId} className={styles.popover}>
+        <strong className={accentFont.className}>{key}</strong>:{' '}
+        <em className={accentFont.className}>{def.partOfSpeech}</em>
+        <br />
+        {def.definition}
+      </span>
+    </span>
   );
 };
